9e9ed0243346d31cfc501e85e096ff11a0374087
EBIZBRO <ebizbrollp@gmail.com>
2026-04-26 18:09:03 +0530

    Ready for deployment

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHO_YxgwInSN7zSmgP-IbwKachQSZZUH8",
-  authDomain: "ebizbro-llp.firebaseapp.com",
-  projectId: "ebizbro-llp",
-  storageBucket: "ebizbro-llp.appspot.com",
-  messagingSenderId: "266808578165",
-  appId: "1:266808578165:web:85b17e7b6d3105517ea83c",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 
